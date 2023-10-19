e18bce039b77254f5f342116fc5787d3a6f7c206
Tommy-La <tien.la@edu.sait.ca>
2023-10-19 16:20:41 -0600

    finished week4, added new-item.js and page.js

---
+++ app/week4/page.js
@@ -0,0 +1,14 @@
+'use client';
+import NewItem from "./new-item";
+import React from 'react';
+
+
+export default function page(){
+    return(
+    <main class="flex justify-center w-full">
+        <NewItem/>
+    </main>
+    )
+}
+
+
