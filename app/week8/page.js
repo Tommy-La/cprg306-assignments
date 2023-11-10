e6e123d52d345593731f9a67c4db1d5a92c6279f
Tommy-La <tien.la@edu.sait.ca>
2023-11-09 20:27:28 -0700

    Update week 8 page.js

--- app/week8/page.js
+++ app/week8/page.js
@@ -1,11 +1,42 @@
+"use client"
+import { useEffect } from 'react';
 import { useUserAuth } from "./_utils/auth-context";
- 
-const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
- 
-await gitHubSignIn();
- 
-await firebaseSignOut();
- 
-<p>
-  Welcome, {user.displayName} ({user.email})
-</p>;
+
+export default function LandingPage() {
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  const handleGitHubSignIn = async () => {
+    await gitHubSignIn();
+  };
+
+  const handleFirebaseSignOut = async () => {
+    await firebaseSignOut();
+  };
+
+  useEffect(() => {
+    // You can put any initialization code here that needs to run on the client
+    // For example, you might want to automatically sign in the user when the component mounts
+    handleGitHubSignIn();
+  }, []); // Empty dependency array ensures that this effect runs only once on mount
+
+  return (
+    <div>
+      {user ? (
+        // User is logged in
+        <div>
+          <p>
+            Welcome, {user.displayName} ({user.email})
+          </p>
+          <button onClick={handleFirebaseSignOut}>Logout</button>
+          <a href="../week8/shopping-list">Go to Shopping List</a>
+        </div>
+      ) : (
+        // User is not logged in
+        <div>
+          <p>Please log in to access the application</p>
+          <button onClick={handleGitHubSignIn}>Login with GitHub</button>
+        </div>
+      )}
+    </div>
+  );
+}
