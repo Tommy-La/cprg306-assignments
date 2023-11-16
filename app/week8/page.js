93144f41b8a1b363da06bb85f1d2395417432274
Tommy-La <tien.la@edu.sait.ca>
2023-11-16 14:30:28 -0700

    week 8 log in is complete

--- app/week8/page.js
+++ app/week8/page.js
@@ -14,15 +14,13 @@ export default function LandingPage() {
   };
 
   useEffect(() => {
-    // You can put any initialization code here that needs to run on the client
-    // For example, you might want to automatically sign in the user when the component mounts
+
     handleGitHubSignIn();
-  }, []); // Empty dependency array ensures that this effect runs only once on mount
+  }, []);
 
   return (
     <div>
       {user ? (
-        // User is logged in
         <div>
           <p>
             Welcome, {user.displayName} ({user.email})
@@ -31,7 +29,6 @@ export default function LandingPage() {
           <a href="../week8/shopping-list">Go to Shopping List</a>
         </div>
       ) : (
-        // User is not logged in
         <div>
           <p>Please log in to access the application</p>
           <button onClick={handleGitHubSignIn}>Login with GitHub</button>
